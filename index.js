4769c1abc48b53aa83fa279fabf82a98032f8273
Denex A Poulose <denexpoulose@gmail.com>
2024-10-04 06:21:08 +0530

    Replace token logic with JWT

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "JWT_SECRET";
 const app = express();
 app.use(express.json());
 
@@ -7,79 +8,79 @@ const users = [];
 
 // Return a random long string which is **Token**
 
-const generateToken = () => {
-  let options = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ];
+// const generateToken = () => {
+//   let options = [
+//     "A",
+//     "B",
+//     "C",
+//     "D",
+//     "E",
+//     "F",
+//     "G",
+//     "H",
+//     "I",
+//     "J",
+//     "K",
+//     "L",
+//     "M",
+//     "N",
+//     "O",
+//     "P",
+//     "Q",
+//     "R",
+//     "S",
+//     "T",
+//     "U",
+//     "V",
+//     "W",
+//     "X",
+//     "Y",
+//     "Z",
+//     "a",
+//     "b",
+//     "c",
+//     "d",
+//     "e",
+//     "f",
+//     "g",
+//     "h",
+//     "i",
+//     "j",
+//     "k",
+//     "l",
+//     "m",
+//     "n",
+//     "o",
+//     "p",
+//     "q",
+//     "r",
+//     "s",
+//     "t",
+//     "u",
+//     "v",
+//     "w",
+//     "x",
+//     "y",
+//     "z",
+//     "0",
+//     "1",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//   ];
 
-  let token = "";
+//   let token = "";
 
-  for (let i = 0; i < 32; i++) {
-    token += options[Math.floor(Math.random() * options.length)];
-  }
-  return token;
-};
+//   for (let i = 0; i < 32; i++) {
+//     token += options[Math.floor(Math.random() * options.length)];
+//   }
+//   return token;
+// };
 
 const signUpHandler = (req, res) => {
   const username = req.body.username;
@@ -109,8 +110,13 @@ const signInHandler = (req, res) => {
   });
 
   if (user) {
-    let token = generateToken();
-    user.token = token;
+    let token = jwt.sign(
+      {
+        username: username,
+      },
+      JWT_SECRET
+    );
+    // user.token = token;
     res.json({
       token: token,
     });
@@ -127,11 +133,12 @@ app.post("/signup", signUpHandler);
 app.post("/signin", signInHandler);
 app.get("/me", (req, res) => {
   const token = req.headers.token;
-
+  const decodedInfo = jwt.verify(token, JWT_SECRET);
+  const username = decodedInfo.username;
   let foundUser = null;
 
   for (let i = 0; i < users.length; i++) {
-    if (users[i].token == token) {
+    if (users[i].username == username) {
       foundUser = users[i];
     }
   }
